c8ec1fecf1c9682331627593bd5cf2e35dca190f
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-02-28 16:15:23 +0300

    Set up the configuration settings

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa-access:Access@localhost/pitch'
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuaration settings
+    '''
+
+class TestConfig(Config):
+    '''
+    Testing Configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
